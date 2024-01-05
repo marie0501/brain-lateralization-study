ba0b767979dd8513671b99c0369b645bc2eb3eb5
marie0501 <mariedelvallereyes01@gmail.com>
2024-01-05 10:04:08 -0500

    update docs results

---
+++ src/R/plot_rois_1.R
@@ -0,0 +1,44 @@
+library(ggplot2)
+
+data <- read.csv("C:\\Users\\Marie\\Documents\\thesis\\tables\\tables_rois\\all_rois_table.csv")
+
+data$preferred_period <-log(data$preferred_period)
+data <- data[data$preferred_period > -6,]
+data <- data[data$gml_r2 > 1,]
+data <-data[data$eccen < 6,]
+data$varea <- as.factor(data$varea)
+
+colors <- c("1" = "red", "2" = "darkorange",'3'='gold','4'='lightskyblue','5'='navy','6'='yellowgreen','7'='forestgreen','8'='lightpink','9'='orchid','10'='darkviolet','11'="sienna",'12'='black')
+rois <- c('V1', 'V2', 'V3', 'hV4', 'VO1', 'VO2', 'LO1', 'LO2', 'TO1','TO2','V3b','V3a')
+y_label <-  "Log Período Preferido"
+x_label <- "Excentricidad"
+leyend_label <- "Áreas"
+
+
+g <- ggplot(data,aes(x=eccen,y=preferred_period, color = varea)) + 
+  geom_smooth(method='lm', se=TRUE, aes(fill=varea)) + 
+  theme_bw() + labs(
+    x = x_label,
+    y = y_label
+  ) + 
+  scale_color_manual(
+    name = leyend_label,
+    labels = rois,
+    values = colors
+  ) +
+  scale_fill_manual(
+    name = leyend_label,
+    labels = rois,
+    values = colors
+  ) + theme(axis.text.x = element_text(size = 12),   # Ajusta el tamaño de las etiquetas del eje x
+            axis.text.y = element_text(size = 12),
+            axis.title.x = element_text(size = 18),
+            axis.title.y = element_text(size = 18),
+            legend.text = element_text(size = 14),
+            legend.title = element_text(size = 16)
+  )
+#+
+#  xlim(1, 6) +  # Establecer límites en el eje x
+#  ylim(-1,1)
+
+show(g)
--- src/R/table.R
+++ src/R/table.R
@@ -7,15 +7,15 @@ library(ggplot2)
 library(lme4)
 library(sjPlot)
 
-result_dir <- "C:\\Users\\Marie\\Documents\\thesis\\images\\"
-tables_dir <- "C:\\Users\\Marie\\Documents\\thesis\\tables\\"
+result_dir <- "C:\\Users\\Marie\\Documents\\thesis\\"
+tables_dir <- "C:\\Users\\Marie\\Documents\\thesis\\tables\\tables_rois\\"
 rois <- c('V1', 'V2', 'V3', 'hV4', 'VO1', 'VO2', 'LO1', 'LO2', 'TO1','TO2','V3b','V3a')
 tab <- NULL
 
 for (iroi in 1:12)
 {
   print(iroi) 
-  data <- read.csv(paste0(tables_dir,rois[iroi],"_table_all_cleaned.csv"))
+  data <- read.csv(paste0(tables_dir,rois[iroi],"_table_all.csv"))
   data$preferred_period <-log(data$preferred_period)
   data <- data[data$preferred_period > -6,]
   data <- data[data$gml_r2 > 1,]
@@ -44,8 +44,8 @@ for (iroi in 1:12)
   temp <- cbind(rn,coeff['eccen',],coeff['side',],coeff['eccen:side',])
   cn <- colnames(temp)
   cn[2:5] <- paste0('Excentricidad.',cn[2:5])
-  cn[6:9] <- paste0('Lado.',cn[6:9])
-  cn[10:13] <- paste0('Excentricidad:Lado.',cn[10:13])
+  cn[6:9] <- paste0('Hemisferio.',cn[6:9])
+  cn[10:13] <- paste0('Excentricidad:Hemisferio.',cn[10:13])
   colnames(temp)<-cn
   tab <- rbind(tab,temp)
   
@@ -54,26 +54,35 @@ nt <- nice_table(tab,separate.header = TRUE)
 
 ft <- flextable::autofit(nt, add_w = 0, add_h = 0)
 w<-flextable::width(ft,width=0.3)
-flextable::save_as_image(w, path = paste0(result_dir,'table5',".png"))
+flextable::save_as_image(w, path = paste0(result_dir,'table_pp',".png"))
 
 #dt <- flextable::dim_pretty(nt)
 
 
-#save(tab,file = paste0(result_dir,'tab_all_rois'))
+save(tab,file = paste0(result_dir,'tab_pp.csv'))
 
-#df <- data.frame(x = factor(tab$roiname,levels = tab$roiname), y = c(tab$eccen.Coef,tab$side.Coef,tab$`eccen:side.Coef`), group=rep(c('Excentricida','Lado','Excentricidad:Lado'),each=12))
+df <- data.frame(x = factor(tab$áreas,levels = tab$áreas), y = c(tab$Excentricidad.Coef,tab$Hemisferio.Coef,tab$`Excentricidad:Hemisferio.Coef`),group=rep(c('Excentricidad','Hemisferio','Excentricidad:Hemisferio'),each=12))
+g <- ggplot(df, aes(x=x, y=y, group = group, color=group)) +  
+  geom_line(linewidth = 1.5) + 
+  geom_point(size=5, shape = 16) +
+  labs(
+    x = 'Áreas visuales',
+    y= 'Tamaño de efecto',
+    
+  ) + theme_bw()+
+  scale_color_manual(
+    name = '',
+    labels = c('Excentricidad','Hemisferio','Excentricidad:Hemisferio'),
+    values = c('red','navy','forestgreen')
+  )+theme(axis.text.x = element_text(size = 12),   # Ajusta el tamaño de las etiquetas del eje x
+          axis.text.y = element_text(size = 12),
+          axis.title.x = element_text(size = 16),
+          axis.title.y = element_text(size = 16),
+          legend.text = element_text(size = 14)
+          )
 
-#g <- ggplot(df, aes(x=x, y=y, group = group, color=group)) +  
-#  geom_line(linewidth = 1.5) + 
-#  geom_point(size=5, shape = 16) +
-#  labs(
-#    x = 'Áreas visuales',
-#    y= 'Tamaño de efecto',
-#    
-#  ) + theme_bw()
-#
-#png(paste0(datadir2,'tamanno_efecto_roi.png'),family = "ArialMT", units = "cm",
-#     width = 17.35, height = 15, pointsize = 18, res = 300)
-#show(g)
-#dev.off()
-#
+
+png(paste0(result_dir,'effect_size_pp_coef.png'),family = "ArialMT", units = "cm",
+     width = 17.35, height = 15, pointsize = 18, res = 300)
+show(g)
+dev.off()
